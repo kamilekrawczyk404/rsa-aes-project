5e87b30fcf5f351114fb5f061d2bbe2390c4b773
maysonek <bwmmaj@o2.pl>
2025-11-25 19:28:38 +0100

    +websocket
    prosty websocket wysylajacy asynchronicznie dane, na razie na basic szablonie

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,49 @@
-from fastapi import FastAPI
+import asyncio
+import datetime
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+#na razie prosty html poki frontu nie ma
+html = """
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Test</title>
+</head>
+<body>
+<h1>Dane</h1>
+<button id="start">Start</button>
+<div id="output"></div>
 
+<script>
+let ws;
+document.getElementById("start").onclick = () => {
+    ws = new WebSocket("ws://localhost:8080/ws");
+    ws.onmessage = (event) => {
+        document.getElementById("output").textContent = event.data;
+    };
+};
+</script>
+</body>
+</html>
+"""
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def get():
+    return HTMLResponse(html)
+
+async def przykladowy_timer():
+    await asyncio.sleep(5)
+    return 1
+
+@app.websocket("/ws")
+async def websocket_endpoint(ws: WebSocket):
+    await ws.accept()
+    timer = asyncio.create_task(przykladowy_timer())
+    while not timer.done():
+        data = {"value": datetime.datetime.now().isoformat()}
+        await ws.send_json(data)
+        await asyncio.sleep(0.2)
+    await ws.send_json({"status": "finished"})
