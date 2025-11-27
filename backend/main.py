761d18db2222f6b6158ee9e4e570c929936eae13
maysonek <bwmmaj@o2.pl>
2025-11-27 13:48:14 +0100

    +obluga guzikow start i cancel
    stworzyłem też nowy plik aby rozdzielic funkcjonalnosci serwera i komunikacji z frontem od taskow wykonywanych na backendzie

--- backend/main.py
+++ backend/main.py
@@ -1,43 +1,41 @@
 import asyncio
-import datetime
 from fastapi import FastAPI, WebSocket
-from fastapi.responses import HTMLResponse
-import psutil
+import tasks
+
 
 app = FastAPI()
 
-#zamiast tego w pozniejszym czasie funkcja szyfrujaca
-async def przykladowy_timer():
-    await asyncio.sleep(5)
-    return 1
 
 @app.websocket("/ws")
 async def websocket_endpoint(ws: WebSocket):
     await ws.accept()
+    current_task = None
+    enc_mode = "aes"
+    while True:
+        message = await ws.receive_json()
 
-    timer = asyncio.create_task(przykladowy_timer())
-    sequence = 0
-    task_start = datetime.datetime.now()
-    while not timer.done():
-        current_usage_package = {
-            "type" : "status",
-            "sequence":  sequence,
-            "timestamp": datetime.datetime.now().isoformat(),
-            "data":{
-                "cpu_usage_percent": psutil.cpu_percent(),
-                "mem_total": psutil.virtual_memory().total,
-                "mem_usage": psutil.virtual_memory().used,
-                "mem_usage_percent": psutil.virtual_memory().percent
-            }
-        }
-        await ws.send_json(current_usage_package)
-        sequence += 1
-        await asyncio.sleep(0.2)
-    time_elapsed = datetime.datetime.now() - task_start
-    await ws.send_json({
-            "type" : "summary",
-            "sequence":  sequence,
-            "timestamp": datetime.datetime.now().isoformat(),
-            "time_elapsed": time_elapsed.total_seconds()
-        })
-    sequence = 0
+        if message["type"] == "change_mode":
+            if current_task is None or current_task.done():
+                enc_mode = message["enc_mode"]
+                await ws.send_json({"type": "mode_changed"})
+            else:
+                await ws.send_json({"type": "error",
+                                    "message": "cannot_change_mode_while_running"})
+
+        elif message["type"]=="start":
+            if current_task is None or current_task.done():
+                current_task = asyncio.create_task(tasks.run_with_status(ws, enc_mode))
+                await ws.send_json({"type":"started"})
+            else:
+                await ws.send_json({"type":"already_running"})
+
+        elif message["type"]=="cancel":
+            if current_task and not current_task.done():
+                current_task.cancel()
+                await ws.send_json({"type":"canceling"})
+            else:
+                await ws.send_json({"type":"not_running"})
+
+
+
+
